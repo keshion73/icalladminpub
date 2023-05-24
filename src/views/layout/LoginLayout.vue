933c82e7cad556318782eb2463a2b8f7a58930a9
keshion <keshion@daum.net>
2023-05-24 15:59:04 +0900

    init

---
+++ src/views/layout/LoginLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container>
+    <router-view></router-view>
+  </v-container>
+</template>
+<script>
+// import AppBarBlurAuth from "@/components/AppBarBlurAuth";
+import { FadeTransition } from "vue2-transitions";
+
+export default {
+  name: "page-layout",
+  components: {
+    // AppBarBlurAuth,
+    FadeTransition,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/views/login/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login-cont">
+        로그인
+    </div>
+</template>
+<script>
+
+export default {
+    components: {
+    },
+    data() {
+        return {
+        };
+    },
+};
+</script>
