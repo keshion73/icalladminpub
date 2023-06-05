f46cb370d70cb8bc98bc46784e5f7fc808e02589
kimhyuna <hyunaa514@naver.com>
2023-06-05 12:42:42 +0900

    관리자 로그인페이지

--- src/App.vue
+++ src/App.vue
@@ -6,3 +6,7 @@
 export default {
 };
 </script>
+
+<style lang="scss">
+  @import '@/assets/scss/common.scss';
+</style>
--- src/views/layout/LoginLayout.vue
+++ src/views/layout/LoginLayout.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-container>
-    <router-view></router-view>
+  <v-container fluid>
+      <router-view></router-view>
   </v-container>
 </template>
 <script>
-// import AppBarBlurAuth from "@/components/AppBarBlurAuth";
-import { FadeTransition } from "vue2-transitions";
 
 export default {
   name: "page-layout",
   components: {
-    // AppBarBlurAuth,
-    FadeTransition,
   },
   data() {
     return {};
   },
 };
 </script>
+
+<style lang="scss" scoped>
+
+</style>
